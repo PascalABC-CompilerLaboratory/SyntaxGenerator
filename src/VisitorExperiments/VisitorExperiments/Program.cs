2aefc36afa4b6d91274e9b3a733fac5657c6cee7
Alexander Zakharenko <wisestump@yandex.ru>
2016-03-24 00:54:18 +0300

    Создал проект с визиторами, добавил визиртор группировки перегруженных процедур

---
+++ src/VisitorExperiments/VisitorExperiments/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PascalABCCompiler.PascalABCNewParser;
+using PascalABCCompiler.SyntaxTree;
+using PascalABCSavParser;
+using SyntaxVisitors;
+
+namespace VisitorExperiments
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string program =
+            @"procedure A(b: char);
+            begin
+            end;
+
+            procedure A(b: string);
+            begin
+            end;
+
+            procedure C(c: string);
+            begin
+            end;
+
+            procedure B(a: char);
+            begin
+            end;
+
+            procedure A(b: integer);
+            begin
+            end;
+
+            procedure B(a: integer);
+            begin
+            end;
+
+            begin
+            end.";
+
+            PascalABCNewLanguageParser parser = new PascalABCNewLanguageParser();
+            syntax_tree_node root = parser.BuildTreeInNormalMode("NoName", program);
+
+            GroupNodesVisitor groupVisitor = new GroupNodesVisitor();
+            root.visit(groupVisitor);
+
+            SimplePrettyPrinterVisitor prettyPrinter = new SimplePrettyPrinterVisitor();
+            root.visit(prettyPrinter);
+
+            Console.ReadKey();
+        }
+    }
+}
